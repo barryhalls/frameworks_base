9ed0e922c2ff1edb30ddaf7be0e60cf301b2bc69
ar <krarvind@gmail.com>
2012-03-16 11:28:28 -0300

    Added Weather support, lockscreen wallpaper support to octo unlock, SystemUI changes to support weather

--- packages/SystemUI/src/com/android/systemui/statusbar/policy/WeatherText.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/policy/WeatherText.java
@@ -10,7 +10,6 @@ import android.database.ContentObserver;
 import android.os.Handler;
 import android.provider.Settings;
 import android.util.AttributeSet;
-import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
@@ -19,28 +18,31 @@ public class WeatherText extends TextView {
     private boolean mAttached;
 
     public static final String EXTRA_CITY = "city";
-    public static final String EXTRA_ZIP = "zip";
     public static final String EXTRA_CONDITION = "condition";
-    public static final String EXTRA_FORECAST_DATE = "forecase_date";
-    public static final String EXTRA_TEMP_F = "temp_f";
-    public static final String EXTRA_TEMP_C = "temp_c";
+    public static final String EXTRA_FORECAST_DATE = "forecast_date";
+    public static final String EXTRA_TEMP = "temp";
     public static final String EXTRA_HUMIDITY = "humidity";
     public static final String EXTRA_WIND = "wind";
     public static final String EXTRA_LOW = "todays_low";
     public static final String EXTRA_HIGH = "todays_high";
 
+    private static boolean showLocation = false;
+
     BroadcastReceiver weatherReceiver = new BroadcastReceiver() {
         @Override
         public void onReceive(Context context, Intent intent) {
-            setText(intent.getCharSequenceExtra("temp") + ", "
+            String wText = (showLocation) ? (intent.getCharSequenceExtra(EXTRA_CITY) + ", "
+                    + intent.getCharSequenceExtra(EXTRA_TEMP) + ", "
+                    + intent.getCharSequenceExtra(EXTRA_CONDITION)) : (intent
+                    .getCharSequenceExtra(EXTRA_TEMP) + ", "
                     + intent.getCharSequenceExtra(EXTRA_CONDITION));
+            setText(wText);
         }
     };
 
     public WeatherText(Context context, AttributeSet attrs) {
         super(context, attrs);
         setText("");
-
     }
 
     @Override
@@ -48,7 +50,7 @@ public class WeatherText extends TextView {
         super.onAttachedToWindow();
         if (!mAttached) {
             mAttached = true;
-            IntentFilter filter = new IntentFilter("com.aokp.romcontrol.INTENT_WEATHER_UPDATE");
+            IntentFilter filter = new IntentFilter("com.aokp.slimsettings.INTENT_WEATHER_UPDATE");
             getContext().registerReceiver(weatherReceiver, filter, null, getHandler());
 
             SettingsObserver so = new SettingsObserver(getHandler());
@@ -75,6 +77,12 @@ public class WeatherText extends TextView {
             resolver.registerContentObserver(
                     Settings.System.getUriFor(Settings.System.USE_WEATHER), false,
                     this);
+            resolver.registerContentObserver(
+                    Settings.System.getUriFor(Settings.System.WEATHER_SHOW_LOCATION), false,
+                    this);
+            resolver.registerContentObserver(
+                    Settings.System.getUriFor(Settings.System.WEATHER_STATUSBAR_STYLE), false,
+                    this);
             updateSettings();
         }
 
@@ -87,7 +95,10 @@ public class WeatherText extends TextView {
     protected void updateSettings() {
         ContentResolver resolver = mContext.getContentResolver();
 
-        boolean useWeather = Settings.System.getInt(resolver, Settings.System.USE_WEATHER, 0) == 1;
+        boolean useWeather = Settings.System.getInt(resolver, Settings.System.USE_WEATHER, 0) == 1
+                && Settings.System.getInt(resolver, Settings.System.WEATHER_STATUSBAR_STYLE, 1) == 0;
+
+        showLocation = Settings.System.getInt(resolver, Settings.System.WEATHER_SHOW_LOCATION, 0) == 1;
         setVisibility(useWeather ? View.VISIBLE : View.GONE);
     }
 
